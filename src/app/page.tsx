59ffb7489097c179b06c57212e6f56a89cb08c3a
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-06-02 05:35:41 +0300

    enhance movie type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 "use client";
 import { fetchAllMovies } from "@/fetchapi";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
+interface Movie {
+  id: number;
+  title: string;
+  release_date: string;
+  poster_path?: string;
+}
+
 export default function Home() {
-  const [movies, setMovies] = useState<any[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchAllMovies()
-      .then((results: any[]) => setMovies(results))
+      .then((results: Movie[]) => setMovies(results))
       .finally(() => setLoading(false));
   }, []);
 
@@ -21,6 +29,16 @@ export default function Home() {
         <ul>
           {movies.map((movie) => (
             <li key={movie.id}>
+              <Image
+                src={
+                  movie.poster_path
+                    ? `https://image.tmdb.org/t/p/w200${movie.poster_path}`
+                    : "/placeholder.png"
+                }
+                alt={movie.title}
+                width={100}
+                height={150}
+                unoptimized  />
               {movie.title} ({movie.release_date})
             </li>
           ))}
