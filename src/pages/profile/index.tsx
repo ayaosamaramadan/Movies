99f56000bde54c413fe0bb48771126d6b2be9c40
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-06-19 00:02:46 +0300

    change project url

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -8,6 +8,7 @@ export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
   if (!session) {
     return {
+      
       redirect: {
         destination: "/auth",
         permanent: false,
