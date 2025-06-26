92eceb873eefb7590da49ca4fb3b96fbac63320f
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-06-27 00:52:06 +0300

    fix: Add missing image domains to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ["avatars.githubusercontent.com",
-      'via.placeholder.com'
+    domains: [
+      "avatars.githubusercontent.com",
+      "via.placeholder.com",
+      "lh3.googleusercontent.com", // Google profile images
+      "image.tmdb.org", // TMDb movie images
     ],
   },
 };
